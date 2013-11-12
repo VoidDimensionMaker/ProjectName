08d8a35751e66d852a940b79692ad8cd43711949
VoidDimensionMaker <voiddimensionmaker@gmail.com>
2013-11-12 10:30:01 +0200

    Initial Commit

---
+++ ProjectName/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProjectName
+//
+//  Created by VoidDimensionMaker on 12.11.13.
+//  Copyright (c) 2013 vdm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ProjectName/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ProjectName
+//
+//  Created by VoidDimensionMaker on 12.11.13.
+//  Copyright (c) 2013 vdm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
